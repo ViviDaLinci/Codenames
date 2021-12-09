3cf7fb7528da47b3374ceb5256eb7cadf42c8bb4
NRDNK <94864957+NRDNK@users.noreply.github.com>
2021-12-09 15:48:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,123 @@
 import random
+import os
+from enum import Enum
+import math
+import sys
 
-#
-regeln_anzeigen = input("Regeln anzeigen? (j/n)\n")
-if regeln_anzeigen == "j":
-  print("Regeln blablabla")
-spielstart = input("Mit dem Spiel beginnen? (j/n)\n")
+#https://www.delftstack.com/de/howto/python/python-clear-console/
+def clearConsole():
+    command = 'clear' if os.name not in ('nt', 'dos') else 'cls'  # If Machine is running on Windows, use cls
+    os.system(command)
 
-if regeln_anzeigen == "n" or spielstart == "j":
-  # Wortliste aus Datei öffnen
+class State(Enum):
+  START = 0,
+  PREPARE_ROUND = 1,
+  DISPLAY_RULES = 2,
+  PLAY_TURN = 3,
+  PLAY_AGAIN = 4,
+  END = -1,
+
+class Codenames():
   with open("wordlist-eng.txt", "r") as f:
-    f_contents = f.readlines()
-    f_contents = [line.rstrip() for line in f_contents]
+    ganze_wortliste = f.readlines()
+    ganze_wortliste = [line.rstrip() for line in ganze_wortliste]
+  wortliste = random.sample(ganze_wortliste, 25)
 
-  # 25 zufällige Wörter aus der Wortliste auswählen
-  wortliste = random.sample(f_contents, 25)
+  def run(self):
+    """Starts the game."""
+    self.state = State.START
+    while True:
+      if self.state == State.START:
+        print("Hallo! Willkommen bei Codenames! Möchtest du erst die Regeln erfahren? (ja/nein)")
+        response = input('>')
+        if response.lower() == 'ja':
+          self.state = State.DISPLAY_RULES
+        else:
+          self.state = State.PREPARE_ROUND
+        continue
 
-  # 5x5 Wortmatrix anzeigen:
-  #for i in range(5):
-  #  print("\t\t".join(wortliste[i*5:i*5+5]))
-  print(wortliste)
+      if self.state == State.DISPLAY_RULES:
+        self.explain_rules()
+        self.state = State.PLAY_TURN
+        continue
 
-  print("\nTeam Rot ist dran!")
+      if self.state == State.PREPARE_ROUND:
+        self.prepare_round()
+        self.state = State.PLAY_TURN
+        continue
 
-  if spielstart == "n":
-    quit()
+      if self.state == State.PLAY_TURN:
+        self.print_current_game_state()
+        response = self.ask_for_letter()
+        self.state = self.evaluate_answer(response)
+        continue
+
+      if self.state == State.PLAY_AGAIN:
+        self.state = self.ask_to_play_again()
+        continue
+
+      if self.state == State.END:
+        self.announce_winners()
+        print("Okay! Bye-bye!")
+        sys.exit()
+
+  def next_team(self):
+      """Changes the active team."""
+      self.active_team = (self.active_team + 1) % 2
+
+  def prepare_round(self):
+      """Prepares the next round by setting a new word to guess and resetting the guessed letters."""
+      self.next_team()
+      self.word = random.choice(Hangman.possible_words).lower()
+      self.identified_letters = set()
+      self.wrong_letters = set()
+
+  def explain_rules(self):
+      """Prints an explanation of the rules."""
+      clearConsole()
+      print("Das Spiel funktioniert so: Es gibt zwei Spieler und den Computer.\n"
+            "Der Computer denkt sich ein Wort aus und die Spieler müssen es erraten.\n"
+            "Wenn ein Spieler am Zug ist, darf er einen Buchstaben raten.\n"
+            "Wenn der Buchstabe im Wort enthalten ist, darf der Spieler nochmal raten, \n"
+            "ansonsten ist der andere Spieler an der Reihe.\n"
+            "Für jeden falsch geratenen Buchstaben gibt es 1 Strafpunkt.\n"
+            "Wenn ihr das Wort lösen möchtet, schreibt 'lösen'.\n"
+            "Wenn ihr die Regeln nochmal lesen möchtet, schreibt 'Regeln'.\n"
+            "Achtung! Bei falscher Lösung gibt es 3 Strafpunkte! "
+            "Wer das Wort rät, bekommt 5 Strafpunkte abgezogen."
+            "Wer am Ende die wenigsten Strafpunkte hat, gewinnt! , gewinnt! Viel Spaß :)")
+
+  def print_current_game_state(self):
+      """Prints letters already guessed, open letters and player scores."""
+      for i in range(5):
+        print("\t\t".join(wortliste[i * 5:i * 5 + 5]))
+
+  def spymaster(self):
+
+  def ask_for_word(self):
+      """Prints a prompt to guess a word and returns the user's input."""
+      print(f"Team{self.active_team + 1} ist an der Reihe!")
+      print("Welches Wort rätst du?")
+      return input(">").upper()
+
+  def announce_winners(self):
+
+
+  def ask_to_play_again(self):
+    """Asks the players whether they want to play again and returns the corresponding next game state."""
+    again = input("Möchtet ihr nochmal spielen? (ja/nein)")
+    if again.lower().strip() == "ja":
+      return State.PREPARE_ROUND
+    else:
+      return State.END
+
+  def determine_winners(self):
+
+
+
+def main():
+  game = Codenames()
+  game.run()
+
+if __name__ == "__main__":
+    main()
